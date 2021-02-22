07b98278436d06d17dae3842c93a9558e0d01ea7
KamrynCutler <Kammykam0899@gmail.com>
2021-02-22 17:54:27 -0500

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by admin on 2/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
