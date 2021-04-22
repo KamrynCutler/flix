6034684f92145de875d1786b219abe80af6c5440
Daijon B <daijon.bereolacarso@bison.howard.edu>
2021-04-22 13:24:38 -0400

    flix submit

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,12 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by admin on 3/24/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+}
